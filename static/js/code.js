06e99d422e47055328a4218ce74e63431972239c
TheSumsito <mjaral@outlook.com>
2019-12-14 00:45:56 -0300

    modificacion menu

---
+++ static/js/code.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var contador = 0;
+    $('#icono').click(function () {
+        if (contador == 0) {
+            $('#menu').slideDown();
+            contador = 1;
+        } else {
+            $('#menu').slideUp();
+            contador = 0;
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    var altura = $('.container-menu').offset().top;
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() > altura) {
+            $('.container-menu').addClass('container-fixed');
+            $('.menu').addClass('container-fixed');
+        } else {
+            $('.container-menu').removeClass('container-fixed');
+            $('.menu').removeClass('container-fixed')
+        }
+    });
+});
